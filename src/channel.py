5a70bdc4725138a440298910e662752061b14aee
evgeniia lemanova <jane87.05@mail.ru>
2023-03-15 23:06:40 +0300

    add json_file

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
-from helper.youtube_api_manual import channel
+from helper.youtube_api_manual import channel, youtube
+import json
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -8,7 +9,6 @@ class Channel:
         self.__channel_id = channel_id
         self.channel = channel
         items = channel["items"][0]
-        self.id = items["id"]
         self.description = items["snippet"]["description"]
         self.title = items["snippet"]["title"]
         self.url = "https://www.youtube.com/channel/" + "UCMCgOm8GZkHp8zJ6l7_hIuA"
@@ -20,3 +20,29 @@ class Channel:
         """Выводит в консоль информацию о канале."""
         self.channel = channel
         print(self.channel)
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @staticmethod
+    def get_service():
+        '''Возвращает объект для работы с YouTube API'''
+        return youtube
+
+    def to_json(self, json_file):
+        '''Сохраняет в файл значения атрибутов экземпляра `Channel`'''
+        dict_channel = {}
+        dict_channel['id'] = self.channel_id
+        dict_channel['description'] = self.description
+        dict_channel['title'] = self.title
+        dict_channel['url'] = self.url
+        dict_channel['subscriberCount'] = self.subscriberCount
+        dict_channel['video_count'] = self.video_count
+        dict_channel['viewCount'] = self.viewCount
+        with open("vdud.json", "w") as write_file:
+            json.dump(dict_channel, write_file)
+
+
+
+

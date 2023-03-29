83fc847e22b3b32d0b00ec8d8585e3df921a7e19
evgeniia lemanova <jane87.05@mail.ru>
2023-03-29 20:41:18 +0300

    add Homework_5

--- src/playlist.py
+++ src/playlist.py
@@ -2,45 +2,46 @@ from helper.youtube_api_manual import youtube
 import datetime
 import isodate
 
+
 class PlayList:
     '''Класс для получения данных из плейлиста'''
+
     def __init__(self, playlist_id):
         '''Инициализация по ID плейлиста
-        атррибуты: название плейлиста и ссылка на плейлист
+        атррибут: название плейлиста и ссылка на плейлист
         '''
-
         self.__playlist_id = playlist_id
         # Данные плейлиста
         playlist = youtube.playlistItems().list(playlistId=playlist_id,
-                                               part='snippet',
-                                               maxResults=50,
-                                               ).execute()
+                                                part='snippet',
+                                                maxResults=50,
+                                                ).execute()
         # Находим ID канала
         channel_id = playlist['items'][0]['snippet']['channelId']
         # Вытаскиваем все плейлисты канала
         playlists_channel = youtube.playlists().list(channelId=channel_id,
-                                          part='contentDetails,snippet',
-                                          maxResults=50,
-                                          ).execute()
+                                                     part='contentDetails,snippet',
+                                                     maxResults=50,
+                                                     ).execute()
         # Перебираем плейлисты канала чтобы вытащить нужный и вывести название плейлиста
         for playlist in playlists_channel['items']:
             if playlist['id'] == playlist_id:
-                self.title = playlist['snippet']['title']
+                self.__title = playlist['snippet']['title']
                 break
             else:
-                self.title = None
-        self.url = 'https://www.youtube.com/playlist?list=' + self.__playlist_id
+                self.__title = None
+        self.__url = 'https://www.youtube.com/playlist?list=' + self.__playlist_id
 
         # Вытаскиваем все видео для подсчета общей длительности плейлиста
         self.playlist_videos = youtube.playlistItems().list(playlistId=self.__playlist_id,
-                                                       part='contentDetails',
-                                                       maxResults=50,
-                                                       ).execute()
+                                                            part='contentDetails',
+                                                            maxResults=50,
+                                                            ).execute()
         # получить все id видеороликов из плейлиста
         self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
         self.video_response = youtube.videos().list(part='contentDetails,statistics',
-                                               id=','.join(self.video_ids)
-                                               ).execute()
+                                                    id=','.join(self.video_ids)
+                                                    ).execute()
         # Складываем время каждого видео
         self.__total_duration = datetime.timedelta()
         for video in self.video_response['items']:
@@ -53,15 +54,19 @@ class PlayList:
     def total_duration(self):
         return self.__total_duration
 
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def url(self):
+        return self.__url
+
     def show_best_video(self):
+        '''Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)'''
         max_likes = 0
-        url_best_video = ''
-        for video in self.video_response:
-            if video['items']['statistics']['likeCount'] > max_likes:
-                max_likes = int(video['items']['statistics']['likeCount'])
-                url_best_video = "https://youtu.be/" + video['items']['id']
-        return url_best_video
-
-
-a = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
-print(a.show_best_video())
+        for video in self.video_response['items']:
+            if int(video['statistics']['likeCount']) > max_likes:
+                max_likes = int(video['statistics']['likeCount'])
+                self.__url_best_video = "https://youtu.be/" + video['id']
+        return self.__url_best_video
